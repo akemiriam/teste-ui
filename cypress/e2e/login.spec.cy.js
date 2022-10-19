edfc840082d03a3f330ee89fcc0c5a9ec92a7ea1
Akemiriam <miriam.mecelis@gmail.com>
2022-10-19 15:13:36 -0300

    meu primeiro commit

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,38 @@
+/// <reference types="cypress" />
+
+context ('Funcionalidade Login', () =>{
+
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('https://www.netoffice.com.br/ativaempresa.php')
+        cy.get('#bt_entrar_empresa').click()
+        cy.get('#cod_empresa').type('demo')
+        cy.get('#login-modal > .modal-dialog > .modal-content > .modal-body > .login-form-container > .login-form > .btn').click() 
+        cy.get('#login_username').type('brenoabreu1992@gmail.com')
+        cy.get('#login_pass').type('131008Bm')
+        cy.get('#login_toggle').click()
+        cy.get('[style="position: absolute; bottom: 53px; left: 139px;"] > .semi-bold').should('contain', 'Breno Falcão Abreu')
+
+
+    })
+    it('Deve exibir uma mensagem de erro ao inserir usuário inválido', ()=> {
+        cy.visit('https://www.netoffice.com.br/ativaempresa.php')
+        cy.get('#bt_entrar_empresa').click()
+        cy.get('#cod_empresa').type('demo')
+        cy.get('#login-modal > .modal-dialog > .modal-content > .modal-body > .login-form-container > .login-form > .btn').click() 
+        cy.get('#login_username').type('brenoabreu92@gmail.com')
+        cy.get('#login_pass').type('131008')
+        cy.get('#login_toggle').click()
+        cy.get('[href="esqueceu_senha.php"]').should('contain', 'Problemas ao acessar?')
+    })
+
+    it('Deve exibir uma mensagem de erro ao inserir senha inválida', ()=> {
+        cy.visit('https://www.netoffice.com.br/ativaempresa.php')
+        cy.get('#bt_entrar_empresa').click()
+        cy.get('#cod_empresa').type('demo')
+        cy.get('#login-modal > .modal-dialog > .modal-content > .modal-body > .login-form-container > .login-form > .btn').click() 
+        cy.get('#login_username').type('brenoabreu1992@gmail.com')
+        cy.get('#login_pass').type('131008')
+        cy.get('#login_toggle').click()
+    })
+    
+})
